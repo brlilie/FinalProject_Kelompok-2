8150be033c9acd25aa8f6e1ae0327dd2c410e05e
anindiagitaa <168162278+anindiagitaa@users.noreply.github.com>
2024-12-13 09:19:11 +0700

    Create bird.java

---
+++ class/bird.java
@@ -0,0 +1,26 @@
+package projectuap;
+
+public class Bird extends GameObject {
+    private static final double GRAVITY = 1.0;
+    private static final double JUMP_FORCE = -10.0;
+    private double velocityY;
+    
+    public Bird(double x, double y) {
+        super(x, y, 34, 24, "/projectuap/flappybird.png");
+        this.velocityY = 0;
+    }
+    
+    public void jump() {
+        velocityY = JUMP_FORCE;
+    }
+    
+    @Override
+    public void update() {
+        velocityY += GRAVITY;
+        y += velocityY;
+    }
+    
+    public boolean isOutOfBounds(double screenHeight) {
+        return y <= 0 || y >= screenHeight;
+    }
+}
